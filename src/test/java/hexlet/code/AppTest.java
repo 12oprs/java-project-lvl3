2f71e52898ef0708ef6ede69652f20fab9040be6
Alexander L <12oprs@gmail.com>
2021-12-05 04:48:06 +1200

    add test NumberSchema

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -13,7 +13,7 @@ class AppTest {
 
     @Test
     @DisplayName("Test StringSchema")
-    void appTest() {
+    void stringSchemaTest() {
 
         Validator v = new Validator();
         StringSchema schema = v.string();
@@ -22,7 +22,6 @@ class AppTest {
         assertEquals(true, schema.isValid(null));
 
         schema.required();
-
         assertEquals(true, schema.isValid("what does the fox say"));
         assertEquals(true, schema.isValid("hexlet"));
         assertEquals(false, schema.isValid(null));
@@ -30,5 +29,28 @@ class AppTest {
 
         assertEquals(true, schema.contains("what").isValid("what does the fox say"));
         assertEquals(false, schema.contains("whatthe").isValid("what does the fox say"));
+    }
+
+    @Test
+    @DisplayName("Test NumberSchema")
+    void numberSchemaTest() {
+        Validator v = new Validator();
+        NumberSchema schema = v.number();
+
+        assertEquals(true, schema.isValid(null));
+
+        schema.required();
+        assertEquals(false, schema.isValid(null));
+        assertEquals(true, schema.isValid(10));
+        assertEquals(false, schema.isValid("5"));
+
+        assertEquals(true, schema.positive().isValid(10));
+        assertEquals(false, schema.isValid(-10));
+
+        schema.range(5, 10);
+        assertEquals(true, schema.isValid(5));
+        assertEquals(true, schema.isValid(10));
+        assertEquals(false, schema.isValid(4));
+        assertEquals(false, schema.isValid(11));
     }
 }
