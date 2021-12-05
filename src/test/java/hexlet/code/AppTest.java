352402ba6ba13bf412fff3d07f79fa9d77ec090f
Alexander L <12oprs@gmail.com>
2021-12-06 03:38:49 +1200

    add test MapSchema

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -4,19 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
+import hexlet.code.schemas.MapSchema;
+import java.util.Map;
+import java.util.HashMap;
 
 class AppTest {
 
+    private static Validator v;
+
+    @BeforeEach
+    void preparing() {
+        v = new Validator();
+    }
 
     @Test
     @DisplayName("Test StringSchema")
-    @SuppressWarnings("checkstyle:magicnumber")
     void stringSchemaTest() {
 
-        Validator v = new Validator();
         StringSchema schema = v.string();
 
         assertEquals(true, schema.isValid(""));
@@ -27,7 +35,7 @@ class AppTest {
         assertEquals(true, schema.isValid("hexlet"));
         assertEquals(false, schema.isValid(null));
         assertEquals(false, schema.isValid(""));
-        assertEquals(false, schema.isValid(5));
+        assertEquals(false, schema.isValid(2));
 
         assertEquals(true, schema.contains("what").isValid("what does the fox say"));
         assertEquals(false, schema.contains("whatthe").isValid("what does the fox say"));
@@ -37,7 +45,6 @@ class AppTest {
     @DisplayName("Test NumberSchema")
     @SuppressWarnings("checkstyle:magicnumber")
     void numberSchemaTest() {
-        Validator v = new Validator();
         NumberSchema schema = v.number();
 
         assertEquals(true, schema.isValid(null));
@@ -55,5 +62,25 @@ class AppTest {
         assertEquals(true, schema.isValid(10));
         assertEquals(false, schema.isValid(4));
         assertEquals(false, schema.isValid(11));
+    }
+
+    @Test
+    @DisplayName("Test MapSchema")
+    void mapSchemaTest() {
+        MapSchema schema = v.map();
+
+        assertEquals(true, schema.isValid(null));
+
+        schema.required();
+        assertEquals(false, schema.isValid(null));
+        assertEquals(true, schema.isValid(new HashMap()));
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        assertEquals(true, schema.isValid(data));
+
+        schema.sizeof(2);
+        assertEquals(false, schema.isValid(data));
+        data.put("key2", "value2");
+        assertEquals(true, schema.isValid(data));
     }
 }
